37394f1b3993bdd93f819cc2ea6776076bcfb729
Alexandra <a.r.chumachenko@gmail.com>
2021-03-03 16:45:07 +0200

    Organization list

--- src/vue/components/main-components-list/ApplicationList.vue
+++ src/vue/components/main-components-list/ApplicationList.vue
@@ -1,5 +1,9 @@
 <template>
-    <span>Application List</span>
+   <div>
+       <!---->
+
+
+   </div>
 </template>
 
 <script>
--- src/vue/components/main-components-list/OrganizationList.vue
+++ src/vue/components/main-components-list/OrganizationList.vue
@@ -1,10 +1,51 @@
 <template>
-    <span>Organization List</span>
+    <div>
+        <!--TODO: organization create-->
+        <div class="mt-2">
+            <b-row>
+                <b-card class="w-100 bg-light">
+                    <b-row>
+                        <b-col class="col-6 ">Назва організації</b-col>
+                        <b-col class="col-5 ">Контактний телефон</b-col>
+                    g    <b-col class="col-1 "/>
+                    </b-row>
+                </b-card>
+            </b-row>
+            <b-row v-for="org in organization" :key="org.id" class="mt-1">
+                <b-card class="w-100">
+                    <b-row>
+                        <b-col class="col-6">{{org.name}}</b-col>
+                        <b-col class="col-5">{{org.phoneNumber}}</b-col>
+                        <b-col class="col-1" @click="$router.push({path: `/organization/${org.id}`})"><b-icon icon="box-arrow-right"/></b-col>
+                    </b-row>
+                </b-card>
+            </b-row>
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "OrganizationList"
+        name: "OrganizationList",
+        data(){
+            return{
+                organization: [
+                    {id: 1,
+                        name: 'First Organization',
+                        address: 'Address of organization',
+                        phoneNumber: '092-12-12'},
+                    {id: 2,
+                        name: 'First Organization',
+                        address: 'Address of organization',
+                        phoneNumber: '092-12-12'},
+                    {id: 3,
+                        name: 'First Organization',
+                        address: 'Address of organization',
+                        phoneNumber: '092-12-12'}
+                ]
+            }
+        }
+
     }
 </script>
 
