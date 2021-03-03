0558bcd8a25d0d6db817c65ba427331685532413
Alexandra <a.r.chumachenko@gmail.com>
2021-03-03 19:16:12 +0200

    Add file for created form

---
+++ src/vue/components/inner-components/created-forms/FormApplicationCreated.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "FormApplicationCreated"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/vue/components/inner-components/created-forms/FormEventCreated.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "FormEventCreated"
+    }
+</script>
+
+<style scoped>
+
+</style>
